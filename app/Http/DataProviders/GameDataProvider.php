847c10a5745da86074df2d3ee6a57a8895768326
SurenAvag <s404747@gmail.com>
2017-12-26 03:06:25 +0400

    set sql mode

--- app/Http/DataProviders/GameDataProvider.php
+++ app/Http/DataProviders/GameDataProvider.php
@@ -14,6 +14,7 @@ class GameDataProvider
 
     public function prepareData(): self
     {
+        DB::statement("SET sql_mode = ''");
         $questionIds = DB::select("select id FROM (SELECT id, text, topic_id FROM questions ORDER BY RAND()) AS subquery GROUP BY topic_id");
 
         $this->game = Game::create([])->fresh();
