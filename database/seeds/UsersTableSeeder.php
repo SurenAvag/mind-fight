1f164913ce49afadf478fea0ca26821c3f9d33f1
luizamshetsyan <--global>
2018-05-21 14:00:45 +0400

    Seeder changes.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,6 +10,7 @@ class UsersTableSeeder extends Seeder
             'first_name'    => 'Suren',
             'last_name'     => 'Avagyan',
             'email'         => 's404747@gmail.com',
+            'type'          => \App\Models\User::TYPES['student'],
             'password'      => bcrypt('asdasd'),
             'api_token'     => 'yRKb5YNnW9aq0RS7wL2bLQmkFgiBJCEiXxMdRBM2ynu1Oh6Dng',
         ]);
@@ -18,8 +19,18 @@ class UsersTableSeeder extends Seeder
             'first_name'    => 'Tigran',
             'last_name'     => 'Gevorgyan',
             'email'         => 'tigrangevorgyan95@gmail.com',
+            'type'          => \App\Models\User::TYPES['student'],
             'password'      => bcrypt('asdasd'),
             'api_token'     => 'E8mihlcovQ4k69uSXlLyL27dooRLX7lASVmcmkiBt0gTKLuPV0'
+        ]);
+
+        factory(\App\Models\User::class)->create([
+            'first_name'    => 'Narine',
+            'last_name'     => 'Ispiryan',
+            'email'         => 'nispiryan@ysu.am',
+            'type'          => \App\Models\User::TYPES['lecturer'],
+            'password'      => bcrypt('asdasd'),
+            'api_token'     => 'asdmihlcovQ4k69uSXlLyL27dooRLX7lASVmcmkiBt0gTKLuPV0'
         ]);
 
         factory(\App\Models\User::class, rand(10, 20))->create();
